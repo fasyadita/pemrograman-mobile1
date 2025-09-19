fc41b7d90342893e38b1d0c51f32505f75c54dd2
fasyadita <fasyadita1@gmail.com>
2025-09-19 11:16:27 +0700

    first

---
+++ project3/bin/project3.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:project3/project3.dart' as project3;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project3.calculate()}!');
+  
+  var fixedList = List<int>.filled(4,0);
+
+  fixedList[0] = 1;
+  fixedList[1] = 2;
+  fixedList[2] = 3;
+  // fixedList[3] = 4;
+
+  stdout.writeln(fixedList);
+
+}
